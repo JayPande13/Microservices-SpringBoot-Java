316d6d379edfc70d850deaf29dcf8a7f8c5e20f1
Jay Pande <jmaypande@gmail.com>
2022-07-09 00:45:22 +0530

    1st commit

---
+++ src/main/java/com/example/demo/MicroserviceBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBootApplication.class, args);
+		System.out.println("started....");
+	}
+
+}

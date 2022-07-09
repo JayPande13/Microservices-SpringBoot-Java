9ef53147ba339f9a21f0c3aaa163eeb6e96bfaf2
Jay Pande <jmaypande@gmail.com>
2022-07-09 16:58:35 +0530

    updating project -1

--- src/main/java/com/example/demo/MicroserviceBootApplication.java
+++ src/main/java/com/example/demo/MicroserviceBootApplication.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MicroserviceBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceBootApplication.class, args);
-		System.out.println("started....");
+		System.out.println("it is out");
 	}
 
 }
